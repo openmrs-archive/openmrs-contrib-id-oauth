853ac9da42a58d784ab80b1f83ffea00aa886555
Elliott Williams <e@elliottwillia.ms>
2014-07-18 17:40:52 +0000

    Compliance with felixge's styleguide

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var Common = require(global.__commonModule)
-,   app = require('./lib/app')
+var Common = require(global.__commonModule);
+var app = require('./lib/app');
 
 // Require oauth2, which contains goodies pertaining to our OAuth implementation
-Common.module.oauth = require('./lib/oauth2')
+Common.module.oauth = require('./lib/oauth2');
 
 // Load the app and mount it within OpenMRS ID
-Common.app.use(app)
+Common.app.use(app);
 
 // Load the admin page
-require('./lib/admin')
+require('./lib/admin');
